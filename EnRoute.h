3f474201cd58a22c50e3c2035cd51856ba536932
Cindy Najjar <najjar@polytech.unice.fr>
2015-11-11 10:41:21 +0100

    enRoute

---
+++ EnRoute.h
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+
+#ifndef ENROUTE_H
+#define ENROUTE_H
+
+class EnRoute : public Etat {
+
+	public:
+		Etat figer();
+
+};
+#endif
---
+++ enRoute.cpp
@@ -0,0 +1,2 @@
+#include "enRoute.h"
+#include "Robot.h"
