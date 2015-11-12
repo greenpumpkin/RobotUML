470ac385591e5fa8b435df223843c96109fc114e
user <chloe-1903@live.fr>
2015-11-12 21:58:41 +0100

    main avec plus de tests

--- Main.cpp
+++ Main.cpp
@@ -7,15 +7,21 @@ using namespace std;
 
 int main()
 {
+	cout << "___________Etat initial:__________ " << endl;
 	Robot * r0 = new Robot(3, 4);
 	r0->afficher();
 
+	cout << "___________rencontrer plot-saisir:__________ " << endl;
+	Objet o(5);
 	Plot p;
 	r0->rencontrerPlot(p);
+	r0->saisir(o);
 	r0->afficher();
 
-	Objet o(5);
-	r0->saisir(o);
+	cout << "___________poser-tourner-avancer:__________ " << endl;
+	r0->poser();
+	r0->tourner("NORTH");
+	r0->avancer(1,1);
 	r0->afficher();
 
     return 0;
